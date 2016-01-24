59fc3fd1f574465d8bd82f6e178b96e37a5b76de
uanka <ankazeml97@gmail.com>
2016-01-24 23:50:09 +0200

    Adding items
    It works)
    ..almost

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@
  */
 
 $(function(){
-    var LIST = $(".basket");
-    var ITEM_TEMPLATE = $(".item").html();
-    var LIST_TEMPLATE = $(".list_need").html();
+    var BASKET = $(".basket");
+    var ITEM_TEMPLATE ='<div class="item">'+$(".item").html()+'</div>';
+    var LIST_TEMPLATE = '<div class="li list_need">'+$(".li").html()+'</div>';
+    var LIST = $('.list');
 
     $("#input").find("input").focus();
 
+    $("#add").click(function(){
+        var title = $("#input").find(":input").val();
+        addItem(title);
+        $("#input").find(":input").dele;
+    });
+/*
     $(".item").each(function(i, item) {
         item = $(item); //to have access to all jQuery functions
         item.find(".de").click(decrement(item));
@@ -18,11 +25,8 @@ $(function(){
             item.remove();
         });
     });
+*/
 
-    $("#add").click(function(){
-        var title = $("#input").find(":input").val();
-        addItem(title);
-    });
     /*
     function updateNode(node, fn) {
         node.fadeOut(250, function(){
@@ -33,14 +37,15 @@ $(function(){
     */
     function bought(node) {
         node.addClass("bought");
-        //node.find(".name").hide();
+        node.find(".name").hide();
         //node.find(".name").addClass("hidden");
         //node.find(".but").hide();
-        // node.find(".amount").css("margin-left", "30px");
+        node.find(".amount").css("margin-left", "30px");
         node.find(".name_crossed").show();
         node.find(".close").hide();
-        node.find(".buy").text("Не куплено");
-        node.find(".buy").click(notBought(node));
+        node.find(".n_buy").show();
+        node.find(".buy").hide();
+
         $(".list_need").find(".li").each(function(i, item) {
             var name = item.text();
             if(name == node.find(".name_crossed").text()){
@@ -57,9 +62,11 @@ $(function(){
         node.find(".name").show();
         node.find(".name_crossed").hide();
         */
+        node.find(".amount").css("margin-left", "0px");
         node.find('.close').show();
-        node.find(".buy").text("Куплено");
-        node.find(".buy").click(bought(node));
+        node.find(".buy").show();
+        node.find(".n_buy").hide();
+
         $(".list_done").find(".li").each(function(i, item) {
             var name = item.text();
             if(name == node.find(".name_crossed").text()){
@@ -98,6 +105,7 @@ $(function(){
         });
     }
     function addItem(title) {
+        console.log(ITEM_TEMPLATE);
         var node = $(ITEM_TEMPLATE); //Create new HTML node
         node.addClass("item");
         node.find(".name").val(title); //Set product title
@@ -105,16 +113,21 @@ $(function(){
         node.find(".amount").text(1);
         node.find(".buy").text("Куплено");
 //Bougght Action
-        node.find(".buy").click(bought(node));
+        node.find(".buy").click(function(){
+            bought(node);
+        });
+        node.find(".n_buy").click(function(){
+            notBought(node);
+        });
 //Delete Action
         node.find(".close").click(function(){
             node.remove();
         });
         var linode = $(LIST_TEMPLATE);
-        linode.find(".li").text(title);
+        linode.text(title);
         linode.find(".list_amount").text(2);
-        LIST.append(node); //Add to the end of the list
-        LIST_TEMPLATE.append(linode);
+        BASKET.append(node); //Add to the end of the list
+        LIST.append(linode);
     }
 
 
