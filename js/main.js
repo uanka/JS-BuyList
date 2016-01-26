d342dd3107f5a4ccec59d65aec024e14a04ab1b8
uanka <ankazeml97@gmail.com>
2016-01-26 22:52:47 +0200

    Input&Zoom
    Added Enter key listener.
    Added amount increasing if you add item which already exists.
    Solved problem with correct zoom.
    All buttons work correctly.

--- js/main.js
+++ js/main.js
@@ -7,15 +7,17 @@ $(function(){
     var ITEM_TEMPLATE ='<div class="item">'+$(".item").html()+'</div>';
     var LIST_TEMPLATE = '<div class="li list_need">'+$(".li").html()+'</div>';
     var LIST = $('.shelf');
-    var AMOUNT_CIRCLE = '<span class="list_amount">1</span>';
+//    var AMOUNT_CIRCLE = '<span class="list_amount">1</span>';
 
     $("#input").find("input").focus();
 
-    $("#add").click(function(){
-        var title = $("#input").find(":input").val();
-        addItem(title);
-        $("#input").find(":input").val("");
-
+    $("#add").click(function(){newItem();})
+    $("#input").find(":input").keyup(function(e){
+        if(e.keyCode == 13) {
+            //newItem(); //work
+            $("#add").click();
+            return false;
+        }
     });
 
     $(".item").each(function(i, item) {
@@ -25,6 +27,7 @@ $(function(){
         item.find(".buy").click(function(){bought(item);});
         item.find(".n_buy").click(function(){notBought(item);});
         item.find(".close").click(function(){deleteItem(item);});
+        item.find(".name").keyup(function(){changing(item);})
     });
 
 
@@ -36,12 +39,30 @@ $(function(){
         });
     }
     */
+
+    function newItem() {
+        var title = $("#input").find(":input").val();
+        if (title != "") {
+            var found = false;
+            $(".name_crossed").each(function(i, item) {
+                var name = $(item).text();
+                if (name == title) {
+                    $(this).parent().parent().find(".in").click();
+                    found = true;
+                }
+            });
+            if(!found){
+                    addItem(title);
+                }
+            $("#input").find(":input").val("");
+        }
+        $("#input").find(":input").focus();
+    }
     function bought(node) {
         node.addClass("bought");
         node.find(".name").hide();
-        //node.find(".name").addClass("hidden");
-        //node.find(".but").hide();
-        node.find(".amount").css("margin-left", "30px");
+        node.find(".but").hide();
+       // node.find(".amount").css("margin-left", "30px");
         node.find(".name_crossed").show();
         node.find(".close").hide();
         node.find(".n_buy").show();
@@ -49,7 +70,6 @@ $(function(){
 
         $(".list_need").each(function(i, item) {
             var name = $(item).find(".liname").text();
-            console.log(name);
             if(name == node.find(".name_crossed").text()){
                 var temp = '<div class="li list_done">'+$(item).html()+'</div>';
                 $(item).remove();
@@ -59,13 +79,11 @@ $(function(){
     }
     function notBought(node){
         node.removeClass("bought");
-        /*
         node.find(".but").show();
-        */
         node.find(".name").show();
         node.find(".name_crossed").hide();
 
-        node.find(".amount").css("margin-left", "0px");
+        //node.find(".amount").css("margin-left", "0px");
         node.find('.close').show();
         node.find(".buy").show();
         node.find(".n_buy").hide();
@@ -100,12 +118,22 @@ $(function(){
         }
         old++;
         node.find(".amount").text(old);
-        $(".list_need").find(".liname").each(function(i, item) {
+        $(".liname").each(function(i, item) {
             var name = $(item).text();
             if(name == node.find(".name_crossed").text()){
                 $(this).parent().find(".list_amount").text(old);
             }
         });
+    }
+    function changing(node){
+        var name = node.find(".name_crossed").text();
+        $(".liname").each(function(i, item) {
+            var name = $(item).text();
+            if (name == node.find(".name_crossed").text()) {
+                $(item).text(node.find(".name").val());
+            }
+        });
+        node.find(".name_crossed").text(node.find(".name").val());
     }
     function deleteItem(node){
         node.remove();
@@ -117,10 +145,13 @@ $(function(){
             }
         });
     }
-    function addItem(title) {
+    function addItem(title){
         var node = $(ITEM_TEMPLATE); //Create new HTML node
         node.addClass("item");
         node.find(".name").val(title); //Set product title
+
+        node.find(".name").keyup(function(){changing(node);});
+
         node.find(".name_crossed").text(title);
         node.find(".amount").text(1);
 //Changing amount
@@ -133,7 +164,6 @@ $(function(){
 //Delete Action
         node.find(".close").click(function(){deleteItem(node);});
         var linode = $(LIST_TEMPLATE);
-        console.log(LIST_TEMPLATE);
         linode.find(".liname").text(title);
         BASKET.append(node); //Add to the end of the list
         LIST.append(linode);
